5ea7ecf5e8c3457abe194e205dc3cfdfb0608b16
Chris Taylor <chrismtaylor1998@gmail.com>
2020-07-22 12:40:28 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+// Get all variables
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
+
+
+
+//create a helper function to recieve a list element and add an event listener to toggle on and off done when clicked
+function assignDoneToggle(li){
+	li.addEventListener("click", function(){
+		li.classList.toggle("done")
+	})
+
+}
+
+// helper function to return length of input field
+function inputLength() {
+	return input.value.length;
+}
+
+//Create a list element from the input field
+function createListElement() {
+	var li = document.createElement("li"); // create an li
+	li.appendChild(document.createTextNode(input.value)); //append a child with the value of input
+	assignDoneToggle(li); // assign an event listener 
+	ul.appendChild(li); // append the li to the ul
+	input.value = ""; // reset the input field to empty
+
+}
+
+// create a function to add the list item in input field
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+// create a function to add the list item 
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+// add event listeners to button and input
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+
+
+// Iterate through all existing list elements
+li.forEach(function(li){ // iterate through all li
+	assignDoneToggle(li); // item clickable 
+	addDeleteButton(li); //item has delete button
+})
+
+// add delete buttons and when clicked on, they delete the corresponding items
+function addDeleteButton(li){
+	pass
+}
+
+
+
+
+
+
+
+
+
+
